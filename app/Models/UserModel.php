d9ecb06f8ef8f771ac341467969c25d17e958e0d
Albani Rajata Malik <albeebro002@gmail.com>
2024-09-19 18:30:01 +0700

    Jobsheet 4 - Praktikum 2.4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,5 +12,5 @@ class UserModel extends Model
     protected $table = 'm_user'; // Mendefinisikan nama tabel yg digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan pk dari tabel yg digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
