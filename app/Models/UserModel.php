e396f13a7ef867f74e7d75ba3c7b62d128889219
Albani Rajata Malik <albeebro002@gmail.com>
2024-10-10 23:40:08 +0700

    Jobsheet 7 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,19 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user'; // Mendefinisikan nama tabel yg digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // Mendefinisikan pk dari tabel yg digunakan
+    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo {
+    protected $hidden = ['password']; 
+    protected $casts = ['password' => 'hashed'];
+
+    public function level():BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
